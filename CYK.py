090d4188af35265c25fdbb958bf31e2add10cfac
leandrosfarias <leandro.sfarias997@gmail.com>
2022-06-06 13:26:45 -0300

    2°...

--- CYK.py
+++ CYK.py
@@ -17,22 +17,13 @@ def CYK(gramatica: Gramatica, palavra: str):
     # print(tabela)
     #
     for i in range(1, n):
-        # print(f'valor de n: {n}')
-        # print(f'valor de i: {i}')
-        for j in range(n - i + 1):
-            # print(f'valor de n: {n}')
-            # print(f'valor de i: {j}')
-            print(n - i + 1)
-            for k in range(j - 1):
+        # print(f'i == {i}')
+        for j in range(n - 2 - i + 1):
+            # print(f'n == {n}')
+            # print(f'n - 2 - i + 1 == {n - 2 - i + 1}')
+            for k in range(i - 1):
                 for regra in gramatica.regras:
                     if re.match('^[A-Z]{2}$', regra[1]):
-                        pass
-                        # Y = tabela[k][j]
-                        # Z = regra[i - k, j + k]
-                        # r = Y + Z
-                        # if r in gramatica.regras:
-                        #     tabela[i][j] = r
-                        # tabela[i][j] = '-'
-
-
-
+                        Y = tabela[k][j]
+                        Z = tabela[i - k]
+                        print(f'Y == {Y} e Z == {Z}')
